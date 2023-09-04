1942431c15a1aacfab95934ab86dab0ea1fdb847
gafleury <gabrielfleury95@gmail.com>
2023-09-04 13:16:57 -0300

    api gateway responde adjustment

--- api/main.py
+++ api/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import json
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": json.dumps({
+            "message": "Hello World"
+        }),
+        "isBaseEncoded64": false
+        }
 
 handler = Mangum(app=app)
